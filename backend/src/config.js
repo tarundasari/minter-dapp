95db20f089ee26b62bc33c99958fc94dd237ab8c
rohin-dasari <rohin.dasari@gmail.com>
2022-07-28 18:42:22 -0500

    Update abi

--- backend/src/config.js
+++ backend/src/config.js
@@ -58,10 +58,10 @@ const MINT_PRICE = 0.00001; // Minting price per NFT. Rinkeby = ETH, Ethereum = 
 const TOKENS_PER_MINT = 2; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = "2022-07-29T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-07-27T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x720Af9bE3aa4E87fEbEbb657C8c7B162199a9f7e"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
